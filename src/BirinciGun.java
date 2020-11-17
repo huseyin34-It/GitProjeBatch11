f8268ce9f78a1ca5d1d34721711fe3dd727b89fa
huseyin34-It <68127607+huseyin34-It@users.noreply.github.com>
2020-11-17 23:07:56 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -2,7 +2,6 @@
 public class BirinciGun {
 
 	public static void main(String[] args) {
-		
 		System.out.println("11:57 - Birinci Gun");
 		System.out.println("ikinci commit");
 		System.out.println("10:11 ikinci gun");
