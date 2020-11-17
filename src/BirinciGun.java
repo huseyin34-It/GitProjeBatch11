d61891bed31611b5f7ddefccd57001ec081af52d
huseyin34-It <68127607+huseyin34-It@users.noreply.github.com>
2020-11-17 23:24:35 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -6,11 +6,9 @@ public class BirinciGun {
 		System.out.println("ikinci commit");
 		System.out.println("10:11 ikinci gun");
 				System.out.println("10:26 - ikinci gun");
-<<<<<<< HEAD
 		System.out.println("10:35 - ikinci gun");
-=======
-		system.out.println("10:35 - ikinci gun");
-		system.out.println("11:03 - github.");
->>>>>>> refs/remotes/origin/master
+		System.out.println("10:35 - ikinci gun");
+		System.out.println("11:03 - github.");
+		System.out.println("11:03 - github.");
 	}
 }
