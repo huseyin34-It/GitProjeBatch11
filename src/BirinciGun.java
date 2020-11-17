57e1bd17726a8aa74049248fe199cba5280f5bea
huseyin34-It <68127607+huseyin34-It@users.noreply.github.com>
2020-11-17 22:38:37 +0300

    Update BirinciGun.java
    ikinci gun 2. version

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -6,6 +6,7 @@ public class BirinciGun {
 		System.out.println("11:57 - Birinci Gun");
 		System.out.println("ikinci commit");
 		System.out.println("10:11 ikinci gun");
-				System.out.println(("10:26 - ikinci gun"));
+				System.out.println("10:26 - ikinci gun");
+		system.out.println("10:35 - ikinci gun");
 	}
 }
